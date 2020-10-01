a3977440e4b51873747be26b79595dcaa8fe26b7
HaadyDevs <abdulhaady07@gmail.com>
2020-10-01 17:25:38 +0530

    Designed search bar with search button

--- lib/screens/search_repos.dart
+++ lib/screens/search_repos.dart
@@ -3,14 +3,58 @@
 
 import 'package:flutter/material.dart';
 
+import '../shared/colors.dart';
+import '../shared/colors.dart';
+
 class Search extends StatelessWidget {
+
   static String routename = 'Search';
+  String searchText;
   @override
   Widget build(BuildContext context) {
+
+    // Used device height and width to make responsive layout
+    final _deviceHeight = MediaQuery.of(context).size.height;
+    final _deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey,
         title: Text('Search tags'),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(
+          vertical: _deviceHeight * 0.04,
+          horizontal: _deviceHeight * 0.04,
+        ),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Search Repository",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8)
+                )
+              ),
+            ),
+            SizedBox(
+              height: _deviceHeight * 0.02,
+            ),
+            RaisedButton(
+              onPressed: (){
+                // Get repos on search pressed
+              },
+              child: Text(
+                "Search",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: whiteColor
+                ),
+              ),
+              color: githubButtonGreen,
+            ),
+          ],
+        ),
       ),
     );
   }
