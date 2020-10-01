2129e56411b644a3d0272d541ef2ae3243cf8233
source <anurag@sudodevs.com>
2020-10-02 00:23:50 +0530

    fixes #2

--- lib/screens/contributors.dart
+++ lib/screens/contributors.dart
@@ -1,62 +1,105 @@
-
 //Screen to view the list of all the contributors of the project selected in home page
 
-
 import 'package:flutter/material.dart';
-
-import '../Widgets/contributor_card.dart';
+import 'package:github_flutter/models/contributor_detail_model.dart';
+import 'package:github_flutter/models/contributors_data_model.dart';
+import 'package:http/http.dart' as http;
 import '../models/contributors_card_model.dart';
+import '../widgets/contributor_card.dart';
 
-class Contributors extends StatelessWidget {
+class Contributors extends StatefulWidget {
   static final String routename = '/Contributors';
-  //List of Contributors who have added their cards
-  List <ContributorCard> cardList = [
-    ContributorCard(userName:'Contributor 1',desc:'xyz',displayImgUrl:'https://www.cnam.ca/wp-content/uploads/2018/06/default-profile.gif',website: 'www.abc.com'),
-  ];
 
   @override
+  _ContributorsState createState() => _ContributorsState();
+}
+
+class _ContributorsState extends State<Contributors>
+    with AutomaticKeepAliveClientMixin {
+  List<ContributorCard> cardList = [];
+  @override
+  void initState() {
+    super.initState();
+    getContributors(username: 'IEEE-VIT', repository: 'hacktoberfest-flutter').then((cards) {
+      setState(() {
+        cardList = cards;
+      });
+    });
+  }
+
+  @override
+  // ignore: must_call_super
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Contributors List')),
+        title: Center(
+          child: Text(
+            'Contributors List',
+          ),
+        ),
         backgroundColor: Colors.grey,
       ),
-      body:Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          ListView.builder(
+      body: cardList.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
               // Get the List of contributors to the project from the GitHub Api
               // Append the details of the card to the cardList
               scrollDirection: Axis.vertical,
               shrinkWrap: true,
               itemCount: cardList.length,
-              itemBuilder:(ctx , index){
+              itemBuilder: (ctx, index) {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: ContCard(userName: cardList[index].userName,dispImg: cardList[index].displayImgUrl,desc: cardList[index].desc,webSite: cardList[index].website,),
+                  child: ContCard(
+                    userName: cardList[index].userName,
+                    dispImg: cardList[index].displayImgUrl,
+                    desc: cardList[index].desc,
+                    webSite: cardList[index].website,
+                  ),
                 );
-              }
-           ),
-        ],
+              }),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        child: Icon(Icons.add),
+        onPressed: addToContributors,
       ),
-       floatingActionButton: FloatingActionButton(
-         backgroundColor: Colors.black,
-         child: Icon(Icons.add),
-         onPressed: addToContributors,
-       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
 
-getContributors(){
-  //TODO: Called when the page opens first
-  //Append the contributor details to the cardList
+Future<List<ContributorCard>> getContributors(
+    {@required String username, @required String repository}) async {
+  const String head = 'https://api.github.com/repos/';
+  const String tail = '/contributors';
+  String url = head + username + '/' + repository + tail;
+  http.Response response = await http.get(url);
+  List<Contributor> contributors = contributorFromJson(response.body);
+  List<ContributorCard> contriCards = [];
+  for (final contributor in contributors) {
+    http.Response contributorResponse =
+        await http.get('https://api.github.com/users/' + contributor.login);
+    ContributorDetail contributorDetail =
+        contributorDetailFromJson(contributorResponse.body);
+    contriCards.add(
+      ContributorCard(
+        userName: contributor.login,
+        desc: contributorDetail.bio ?? '',
+        displayImgUrl: contributorDetail.avatarUrl,
+        website: contributorDetail.blog.isEmpty
+            ? contributorDetail.htmlUrl
+            : contributorDetail.blog,
+      ),
+    );
+  }
+  
+  return contriCards;
 }
 
-addToContributors(){
- // TODO: Trigger an alert box or something similar
- // Ask the user to enter the details
- // Adds the user to contributors list
+addToContributors() {
+  // TODO: Trigger an alert box or something similar
+  // Ask the user to enter the details
+  // Adds the user to contributors list
 }
-
-
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,8 +7,9 @@ import 'info_page.dart';
 import 'contributors.dart';
 import 'search_repos.dart';
 
+
 class HomePage extends StatefulWidget {
-  static String routename  = 'HomePage';
+  static String routename = 'HomePage';
   HomePage({Key key}) : super(key: key);
 
   @override
@@ -16,44 +17,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  static List<Widget> _widgetOptions = <Widget>[
-    Search(), //This loads up the Search screen where the user can search for repositories on GitHub
-    Contributors(), // This loads up the the Contributors card page
-    InfoPage(), // TODO: Make an About Us widget
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text('Search'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.apps),
-            title: Text('Contributors'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info_outline),
-            title: Text('About Us'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: TabBarView(
+          children: [
+            Search(), //This loads up the Search screen where the user can search for repositories on GitHub
+            Contributors(), // This loads up the the Contributors card page
+            InfoPage(), // TODO: Make an About Us widget
+          ],
+        ),
+        bottomNavigationBar: TabBar(
+          labelColor: Colors.amber[800],
+          unselectedLabelColor: Colors.black,
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorPadding: EdgeInsets.all(5.0),
+          indicatorColor: Colors.red,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.search),
+              child: Text('Search'),
+            ),
+            Tab(
+              icon: Icon(Icons.rss_feed),
+              child: Text('Contributors'),
+            ),
+            Tab(
+              icon: Icon(Icons.info_outline),
+              child: Text('About Us'),
+            ),
+          ],
+        ),
       ),
     );
   }
