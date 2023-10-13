96e71f768fa83db502bb9e9f1750c368dbf57ec9
shirshakk <kshirshak76@gmail.com>
2023-10-13 15:45:12 +0530

    Update3.0

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -8,14 +8,13 @@ class ThemeProvider extends ChangeNotifier {
     // backgroundColor: Color(0xfffafafa),
     secondaryHeaderColor: Colors.black,
     primaryColor: Colors.white,
-    scaffoldBackgroundColor: const Color(0xfff5f5f5),
-  );
-  var darkTheme = ThemeData(
-    // backgroundColor: Color(0xff071724),
-    secondaryHeaderColor: const Color(0xff93C2DB),
-    primaryColor: darkModeHactober,
     scaffoldBackgroundColor: const Color(0xff0F253E),
   );
+  var darkTheme = ThemeData(
+      // backgroundColor: Color(0xff071724),
+      secondaryHeaderColor: const Color(0xff93C2DB),
+      primaryColor: darkModeHactober,
+      scaffoldBackgroundColor: const Color(0Xff1F2933));
   bool get isDarkTheme => _isDarkTheme;
 
   void changeTheme() {
