283b47974f09b16e8afdc3ceeca302daacb7fa7d
VertikaBajpai <119621037+VertikaBajpai@users.noreply.github.com>
2023-10-20 19:33:53 +0530

    feat: changed splash screen (#53)
    - fixed splash screen colour and theme
    - added new hacktoberfest assets

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -19,22 +19,34 @@ class Splash extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     Timer(
-      const Duration(seconds: 2),
+      const Duration(seconds: 3),
       () => Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (BuildContext context) => const HomePage()),
       ),
     );
 
-    const color = Color(0xff13253d);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(color: color),
-          child: Center(
-            child: Image.asset(
-              'assets/htbf.png',
-            ),
+          padding: const EdgeInsets.all(8),
+          color: const Color.fromARGB(246, 8, 4, 17),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Image.asset(
+                  'assets/logo.png',
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Container(
+                padding: const EdgeInsets.all(22),
+                child: Image.asset('assets/htbf2.png'),
+              ),
+            ],
           ),
         ),
       ),
