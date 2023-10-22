d29bcd763c2e5471a77faf455420c53dabfba060
Dev Keshwani <dev.keshwani345@gmail.com>
2023-10-22 16:58:26 +0530

    fix: about us page hacktoberfest-image render

--- lib/screens/info_page.dart
+++ lib/screens/info_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hacktoberfest_flutter/shared/dictionary.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -48,7 +47,7 @@ class _InfoPageState extends State<InfoPage> {
                 child: Center(
                   child: SizedBox(
                     height: 200,
-                    child: SvgPicture.network(hacktoberfestImage),
+                    child: Image.network(hacktoberfestImage),
                   ),
                 ),
                 onTap: () => launchUrl(
