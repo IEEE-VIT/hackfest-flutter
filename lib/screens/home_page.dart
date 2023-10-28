8848617743252d44bf420e6b2a8e1391dd5a0bdd
Lomesh Wagh <94422862+Lomesh21@users.noreply.github.com>
2023-10-28 10:56:59 +0530

    Home page const warnings are fixed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hacktoberfest_flutter/screens/bookmark.dart';
 import 'package:hacktoberfest_flutter/screens/info_page.dart';
 import 'package:hacktoberfest_flutter/screens/search_repos.dart';
-
-import '../widgets/drawer.dart';
+import 'package:hacktoberfest_flutter/widgets/drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -20,20 +19,19 @@ class _HomePageState extends State<HomePage> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          leading: null,
           backgroundColor: Theme.of(context).primaryColor,
           elevation: 5.0,
-          title: Text('HackFest', style: TextStyle(color: Colors.black),),
+          title: const Text('HackFest', style: TextStyle(color: Colors.black),),
           //shadowColor: Colors.black26,
           iconTheme: IconThemeData(
             color: Theme.of(context).secondaryHeaderColor,
-          )
+          ),
         ),
-        endDrawer: ClipRRect(
+        endDrawer: const ClipRRect(
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(35),
-                bottomLeft: Radius.circular(35)),
-            child: DrawerWidget()
+                bottomLeft: Radius.circular(35),),
+            child: DrawerWidget(),
         ),
         backgroundColor: Theme.of(context).primaryColor,
         body: const TabBarView(
