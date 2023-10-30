ce784a38aa796ee012052bf09458ea4610c45a27
Lomesh Wagh <94422862+Lomesh21@users.noreply.github.com>
2023-10-30 12:14:44 +0530

    Profile page is added and the app drawer switch color is also changed as it hides due to background color

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,11 @@
 //Routes file to store all the routes of the app
 import 'package:flutter/material.dart';
+import 'package:hacktoberfest_flutter/screens/bookmark.dart';
 import 'package:hacktoberfest_flutter/screens/contributors.dart';
 import 'package:hacktoberfest_flutter/screens/home_page.dart';
 import 'package:hacktoberfest_flutter/screens/info_page.dart';
+import 'package:hacktoberfest_flutter/screens/profile_details.dart';
 import 'package:hacktoberfest_flutter/screens/search_repos.dart';
-import 'package:hacktoberfest_flutter/screens/settings.dart';
-import 'package:hacktoberfest_flutter/screens/bookmark.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   Search.routename: (ctx) => const Search(),
@@ -14,6 +14,7 @@ Map<String, Widget Function(BuildContext)> routes = {
         repoName: '',
       ),
   InfoPage.routename: (ctx) => const InfoPage(),
+  ProfileDetails.routename: (ctx) => const ProfileDetails(),
   // Settings.routename: (ctx) => const Settings(),
   Bookmark.routename: (ctx) => const Bookmark(),
 };
